df1768f77cad1dbae20299a11c5ff3b06486e273
Kostiantyn Pylypenko <k.pylypenko@hotmail.com>
2020-06-06 23:19:36 +0300

    add make mfcc wrapper

--- features/make_mfcc.py
+++ features/make_mfcc.py
@@ -2,9 +2,9 @@ import subprocess
 import argparse
 
 
-def make_mfcc(data_dir):
+def make_mfcc(data_dir, mfcc_dir, log_dir):
     subprocess.run(f'cd $KALDI_ROOT/egs/wsj/s5; . ./path.sh; . ./cmd.sh; '
-                   f'steps/make_mfcc.sh {data_dir}',
+                   f'steps/make_mfcc.sh {data_dir} {mfcc_dir} {log_dir}',
                    shell=True)
 
 
@@ -12,7 +12,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('data_dir_path', help='Path to the kaldi data dir')
+    parser.add_argument('mfcc_dir_path', help='Path to the mfcc dir')
+    parser.add_argument('log_dir_path', help='Path to the log dir')
 
     args = parser.parse_args()
 
-    make_mfcc(args.data_dir_path)
+    make_mfcc(args.data_dir_path, args.mfcc_dir_path, args.log_dir_path)
