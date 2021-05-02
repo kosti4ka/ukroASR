a2b010dc7e8de0fa39018ac2d6e5cf7d08d3efc2
kostya <k.pylypenko@hotmail.com>
2021-05-02 23:01:46 +0300

    update gen lexicon

--- tools/gen_lexicon.py
+++ tools/gen_lexicon.py
@@ -90,10 +90,10 @@ def gen_g2p(word_list, variants=1):
             if pron:
                 lexicon[word] = [pron]
             else:
-                subprocess.call(f'echo {word.encode("utf-8")} >> /tmp/failed_lexicon', shell=True)
+                subprocess.call(f'echo {word} >> /tmp/failed_lexicon', shell=True)
         except:
             # TODO move it to logger
-            subprocess.call(f'echo {word.encode("utf-8")} >> /tmp/failed_lexicon', shell=True)
+            subprocess.call(f'echo {word} >> /tmp/failed_lexicon', shell=True)
 
     return lexicon
 
