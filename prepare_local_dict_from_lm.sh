5da843a8b7f70e560780bf8ee8a5413afb6ac404
Kostiantyn <k.pylypenko@hotmail.com>
2021-05-09 21:25:22 +0000

    split am and lm training

--- prepare_local_dict_from_lm.sh
+++ prepare_local_dict_from_lm.sh
@@ -5,18 +5,18 @@ export PYTHONPATH="${PYTHONPATH}:/data/exp/kostya/ukr_g2p"
 export PYTHONPATH="${PYTHONPATH}:/data/exp/kostya/ukroASR"
 UKROASR_ROOT=/data/exp/kostya/ukroASR
 
-num_jobs=8
+nj=1
 
 echo "$0 $@"  # Print the command line for logging
 
 . utils/parse_options.sh
 
 if [ $# -ne 3 ]; then
-  echo "Usage: prepare_lang.sh <src-dict-dir> <lm-path> <out-dict-dir>"
+  echo "Usage: prepare_local_dict_from_lm.sh <src-dict-dir> <lm-path> <out-dict-dir>"
   echo "<src-dict-dir> should contain the following files:"
   echo " extra_questions.txt  nonsilence_phones.txt  optional_silence.txt  silence_phones.txt"
   echo "options: "
-  echo "     --num-jobs <number of jobs>                     # default: 1."
+  echo "     -nj <number of jobs>                     # default: 1."
   exit 1;
 fi
 
@@ -49,13 +49,15 @@ for file in $src_dict_dir/silence_phones.txt \
 done
 
 # generating vocabulary out of lm
-ngram -lm $lm_path -write_vocab $out_dict_dir/vocab
+ngram -lm $lm_path -write-vocab $out_dict_dir/vocab
+# remove </s> <s> <unk> from vocab
+sed -i '/<*>/d' $out_dict_dir/vocab
 
 # generating lexicon file
-python3.6 $UKROASR_ROOT/tools/gen_lexicon.py -v $dict_dir/vocab -o $dict_dir/lexicon.txt -nj $nj
+python3.6 $UKROASR_ROOT/tools/gen_lexicon.py -v $out_dict_dir/vocab -o $out_dict_dir/lexicon.txt -nj $nj
 
 # extend lexicon with silence phones
-echo '!SIL SIL' >> $dict_dir/lexicon.txt
-echo '<SPOKEN_NOISE> SPN' >> $dict_dir/lexicon.txt
-echo '<UNK> SPN' >> $dict_dir/lexicon.txt
-echo '<NOISE> NSN' >> $dict_dir/lexicon.txt
+echo '!SIL SIL' >> $out_dict_dir/lexicon.txt
+echo '<SPOKEN_NOISE> SPN' >> $out_dict_dir/lexicon.txt
+echo '<UNK> SPN' >> $out_dict_dir/lexicon.txt
+echo '<NOISE> NSN' >> $out_dict_dir/lexicon.txt
